5d0e3a75bcc8d4d4b1d8d4d75fd66fa03aa35aa6
tjrans9248 <a900906@naver.com>
2022-11-25 19:26:31 +0900

    add: 오늘의 알고리즘

---
+++ Algorithm/Lv 0/배열의 평균값.js
@@ -0,0 +1,23 @@
+// 배열의 평균값
+
+// 문제 설명
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+// 입출력 예 #1
+
+// numbers의 원소들의 평균 값은 5.5입니다.
+// 입출력 예 #2
+
+// numbers의 원소들의 평균 값은 94.0입니다.
+
+function solution(numbers) {
+  var answer = numbers.reduce((a, b) => a + b);
+  console.log(answer);
+
+  return answer / numbers.length;
+}
+
+// 배열을 반복한다
+// 배열의 인덱스를 다 더한다.
+// 그 더한 값을 배열의 길이와 나눈다.
+// 더한 값들을 다시 배열에 넣어준다.
